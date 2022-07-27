f743edfd78542c41e5fa7d8dd74019a1885749a9
NinhVanHieu <ninhvanhieu1109@gmail.com>
2022-07-27 22:12:45 +0700

    update

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,10 +7,15 @@ function App() {
   return (
     <Router>
       <h2>Feature</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim,
+        delectus. Mollitia porro eaque temporibus quos. Vero nihil eum ullam
+        quam non at assumenda amet minus inventore vitae atque, tenetur rerum.
+      </p>
       <Routes>
         <Route path="/" element={<List />} />
         <Route path="/add" element={<Add />} />
-        <Route path='/edit' element={<Edit/>} />
+        <Route path="/edit" element={<Edit />} />
       </Routes>
     </Router>
   );
